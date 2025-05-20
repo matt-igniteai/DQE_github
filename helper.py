eb7d43cf575c584529965a5e6b0ce010bedc34bb
muhammet <muhammet@igniteaipartners.com>
2025-05-20 13:52:23 +0100

    update gemini model to flass 2.0

--- helper.py
+++ helper.py
@@ -34,7 +34,7 @@ def get_llm(llm_name):
         if not api_key:
             raise ValueError("GOOGLE_API_KEY not found in environment variables.")
         # Use a suitable Gemini model
-        return ChatGoogleGenerativeAI(model="gemini-2.5-flash-preview-04-17", google_api_key=api_key, temperature=0)
+        return ChatGoogleGenerativeAI(model="gemini-2.0-flash", google_api_key=api_key, temperature=0)
     elif llm_name == "DeepSeek":
         api_key = st.secrets.get("DEEP_API_KEY")
         if not api_key:
